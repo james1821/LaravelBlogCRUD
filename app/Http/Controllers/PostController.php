b07f78e089e14f54aa49eab3766df138244a2d09
james1821 <jamesespinosamark@.com>
2023-12-14 13:14:14 +0800

    added deletepost and updatepost function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,15 @@ use App\Http\Controllers\Controller;
 
 class PostController extends Controller
 {
+    public function deletePost(Post $post){
+        if(auth()->user()->id === $post['user_id']){
+            $post->delete();
+    
+        }
+        return redirect('/');
+      
+    }
+    
     public function createPost(Request $request){
         $PostData = $request->validate(
             [
@@ -19,5 +28,31 @@ class PostController extends Controller
             $PostData['body'] = strip_tags($PostData['body']);
             $PostData['user_id'] = auth()->id();
             Post::create($PostData);
+            return redirect('/');
     }
+
+    public function editPost(Post $post){
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/');
+        }
+            return view('edit-post',['post'=>$post]);
+    }
+
+    public function updatePost(Post $post, Request $request){
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/');
+        }
+             $UpdatedData = $request->validate(
+            [
+                'title' => 'required',
+                'body' => 'required'
+            ]
+            );
+            $UpdatedData['title'] = strip_tags($UpdatedData['title']);
+            $UpdatedData['body'] = strip_tags($UpdatedData['body']);
+           $post->update($UpdatedData);
+            return redirect('/');
+}
+
+
 }
